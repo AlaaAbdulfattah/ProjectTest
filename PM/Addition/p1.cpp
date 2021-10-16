6313b6b76855fbf658a6a7f1864599bde3f09dc5
Alaa Abdulfattah <Alaa.Abdulfattah@aun.edu.eg>
2021-10-17 01:28:37 +0200

    First commit

---
+++ PM/Addition/p1.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	int a, b;
+
+	cout << "Enter two numbers:" << endl;
+	cin >> a >> b;
+
+	cout << endl << a << " + " << b << " = " << a + b << endl;
+
+
+	return 0;
+}
